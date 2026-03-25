48aa9f24478efc4cb06715c31db03b38e59d34f4
Aziret127 <utachi86@gmail.com>
2026-03-25 18:19:13 +0600

    выполнил дз 7

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4l)*d9_3gs$i-oseq+&5_+g&=mpymc_ocp4bja(vc^+=rt=x2b'
+SECRET_KEY = os.getenv("AZIRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
